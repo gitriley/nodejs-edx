dab711fa8c25716a2b0c640cb860f3d4b7a34e3f
Riley <riley@Rileys-MacBook-Pro.local>
2018-04-04 09:09:31 -0700

    module 2

---
+++ Module2/blog-api/routes/index.js
@@ -0,0 +1,7 @@
+const posts = require('./posts.js')
+const comments = require('./comments.js')
+const testMethods = require('./test-methods.js')
+
+module.exports = {
+	posts, comments, testMethods
+}
